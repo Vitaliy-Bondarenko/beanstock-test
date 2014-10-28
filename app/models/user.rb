27911c7dca54b1299bd5ab7cd98d6ce07fc3b51d
Vitaliy Bondarenko <joker-tref@ukr.net>
2014-10-28 11:22:52 +0200

    format files

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ActiveRecord::Base
     user.save!
     end
   end
-
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,11 @@ OmniAuth.config.logger = Rails.logger
 LINKEDIN_KEY    = "77vyha9uxg99kg"
 LINKEDIN_SECRET = "VxOBbzOVvoSv528u"
 
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, LINKEDIN_KEY, LINKEDIN_SECRET
 end
 
 OmniAuth.config.on_failure do |env|
-    new_path = "/auth/failure"
-    Rack::Response.new(["302 Moved"], 302, 'Location' => new_path).finish
+  new_path = "/auth/failure"
+  Rack::Response.new(["302 Moved"], 302, 'Location' => new_path).finish
 end
